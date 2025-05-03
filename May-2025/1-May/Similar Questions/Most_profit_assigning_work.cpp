68e402d803443aa248b56dab2041ca22285772d1
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-04 04:03:49 +0530

    Updated code

--- May-2025/1-May/Similar Questions/Most_profit_assigning_work.cpp
+++ May-2025/1-May/Similar Questions/Most_profit_assigning_work.cpp
@@ -32,9 +32,59 @@ int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int
 
     for(int i = 0; i < m; i++){  // O(m*logn)
         auto it = upper_bound(difficulty.begin(), difficulty.end(), worker[i]) - difficulty.begin();
-        if(it == 0) continue; // workers can't do the tast
+        if(it == 0) continue; // workers can't do the task
         else if(it == n || difficulty[it-1] <= worker[i]) total_profit_earn += maxProfit[it-1];
     }
 
     return total_profit_earn;
+}
+
+//**************************************** Alternate Approach ****************************************//
+
+int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker){
+    int n = difficulty.size();
+    int m = worker.size();      // Total Time complexity: O((n+m)*logn + m*logm)
+
+    priority_queue<pair<int,int>> pq;  // maxheap
+    for(int i = 0; i < n; i++) pq.push({profit[i], difficulty[i]}); // O(n*logn)
+
+    sort(worker.begin(), worker.end(), greater<int>()); // sorting in descending order O(m*logm)
+
+    int i = 0;
+    int total_profit = 0;
+    while(i < m && !pq.empty()){  // O(m*logn)
+        if(pq.top().second > worker[i]) pq.pop();  // this task cannot be done by i-th worker O(logn)
+        else{
+            total_profit += pq.top().first;
+            i++;
+        }
+    }
+
+    return total_profit;
+}
+
+//**************************************** Alternate Approach ****************************************//
+
+int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker){
+    int n = difficulty.size();
+    int m = worker.size();        // Time complexity: O(n*logn + m*logm + n + m)
+    
+    vector<pair<int, int>> vec;
+    for(int i = 0; i < n; i++) vec.push_back({difficulty[i], profit[i]}); // O(n)
+    sort(begin(vec), end(vec));  // O(n*logn)
+    sort(begin(worker), end(worker));  // O(m*logm)
+
+    int totalProfit = 0;
+    int j = 0; //pointing to vec
+    int maxProfit = 0;
+
+    for(int i = 0; i < m; i++){  // O(n+m)
+        while(j < n && worker[i] >= vec[j].first){
+            maxProfit = max(maxProfit, vec[j].second);
+            j++;
+        }
+        totalProfit += maxProfit;
+    }
+
+    return totalProfit;
 }
