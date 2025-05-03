710939ea59a1f1436452f1bff3f8c581579a0196
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-03 23:00:39 +0530

    Removed nested Git repository from staging

--- May-2025/1-May/Similar Questions/Most_profit_assigning_work.cpp
+++ May-2025/1-May/Similar Questions/Most_profit_assigning_work.cpp
@@ -12,24 +12,24 @@ int main(){
 
 int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker){
     int n = difficulty.size();
-    int m = worker.size();
+    int m = worker.size();   // Total Time complexity: O(n) + O(n*logn) + O(m*logn)
 
     vector<pair<int,int>> v;
-    for(int i = 0; i < n; i++) v.push_back({difficulty[i], profit[i]});
-    sort(v.begin(), v.end());
+    for(int i = 0; i < n; i++) v.push_back({difficulty[i], profit[i]});  // O(n)
+    sort(v.begin(), v.end());  // O(n*logn)
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){  // O(n)
         difficulty[i] = v[i].first; 
         profit[i] = v[i].second;
     }
 
     vector<int> maxProfit(n,0);  // ith index indicate the maxProfit till now in the vector
     maxProfit[0] = profit[0];
-    for(int i = 1; i < n; i++) maxProfit[i] = max(maxProfit[i-1], profit[i]);
+    for(int i = 1; i < n; i++) maxProfit[i] = max(maxProfit[i-1], profit[i]); // O(n)
 
     int total_profit_earn = 0;
 
-    for(int i = 0; i < m; i++){
+    for(int i = 0; i < m; i++){  // O(m*logn)
         auto it = upper_bound(difficulty.begin(), difficulty.end(), worker[i]) - difficulty.begin();
         if(it == 0) continue; // workers can't do the tast
         else if(it == n || difficulty[it-1] <= worker[i]) total_profit_earn += maxProfit[it-1];
