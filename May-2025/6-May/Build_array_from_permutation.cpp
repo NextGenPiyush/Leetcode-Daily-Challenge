b62083c211b141efbe589b91f55ae5f8fbb07827
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-07 01:43:47 +0530

    added new approach using O(1) space leetcode-1290

--- May-2025/6-May/Build_array_from_permutation.cpp
+++ May-2025/6-May/Build_array_from_permutation.cpp
@@ -7,16 +7,24 @@ int main(){
 
     
 
-
-
     return 0;
 }
 
 vector<int> buildArray(vector<int>& nums){
     int n = nums.size();
     
-    vector<int> ans(n);
-    for(int i=0; i<n; i++) ans[i] = nums[nums[i]];
+    // vector<int> ans(n);  // Space complexity: o(n)
+    // for(int i = 0; i < n; i++) ans[i] = nums[nums[i]];  // Time complexity: o(n)
+    // return ans;
 
-    return ans;
+    for(int i = 0; i < n; i++){   // time complexity: O(n)
+        int original_value = nums[i];   // space complexity: O(1)
+        int new_value = nums[nums[i]];
+        nums[i] = (new_value % n) * n + original_value;
+    }
+
+    for(int i = 0; i < n; i++) nums[i] = nums[i] % n;
+
+    return nums;
+
 }
