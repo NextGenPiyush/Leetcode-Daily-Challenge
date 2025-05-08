6b427b981a320997d40defe3432d9fe8e70d88fb
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-09 03:27:18 +0530

    Minimum time to reach last room-2 question done

--- May-2025/8-May/Find_minimum_time_to_reach_last_room-II.cpp
+++ May-2025/8-May/Find_minimum_time_to_reach_last_room-II.cpp
@@ -1,30 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef pair<int, pair<int, int>> p;
 
 vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // down, up, right, left
-
-int minTimeToReach(vector<vector<int>>& moveTime);  // Leetcode-3341
+int minTimeToReach(vector<vector<int>>& moveTime);  // Leetcode-3342
 
 int main(){
 
-
+    
 
     return 0;
 }
-
-
 
 int minTimeToReach(vector<vector<int>>& moveTime){
     int m = moveTime.size();
     int n = moveTime[0].size();
 
     vector<vector<int>> result(m, vector<int>(n, INT_MAX)); // Store the minimum time to reach each room
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> minHeap; // Min-heap to get the room with the earliest time
+    priority_queue<p, vector<p>, greater<p>> minHeap; // Min-heap to get the room with the earliest time
 
-    result[0][0] = 0; // Starting point, time is 0
-    minHeap.push({0, {0, 0}}); // Push the starting room into the heap
+    result[0][0] = 0; // Starting point, time is 0 result[i][j] --> represent minimum time to reach from (0,0)
+    minHeap.push({0, {0, 0}}); // Push the starting room into the heap {time, {i,j}}
 
-    while(!minHeap.empty()) {
+    while(!minHeap.empty()){
         int currTime = minHeap.top().first;  // Current time to reach the room
         auto cell = minHeap.top().second;
         int i = cell.first;  // Current row
@@ -34,19 +32,21 @@ int minTimeToReach(vector<vector<int>>& moveTime){
             
         if(i == m - 1 && j == n - 1) return currTime;  //we've reached the bottom-right corner, return the time
 
-        // Explore the 4 adjacent directions
+        // Explore the 4 adjacent directions 
         for(auto &dir : directions){
             int nextRow = i + dir[0];
             int nextCol = j + dir[1];
 
             // Check if the next room is within bounds
-            if(nextRow >= 0 && nextRow < m && nextCol >= 0 && nextCol < n) {
-                // Calculate the time to reach the next room (wait time + movement time)
+            if(nextRow >= 0 && nextRow < m && nextCol >= 0 && nextCol < n){
+                int moveCost = (nextRow + nextCol) % 2 == 0 ? 2 : 1;  
+                // if the distance is odd add extra 1 sec or add 2 sec
+                
                 int wait = max(moveTime[nextRow][nextCol] - currTime, 0);
-                int arrivalTime = currTime + wait + 1;
+                int arrivalTime = currTime + wait + moveCost;
 
                 // If a faster route to the next room is found, update and push to the min-heap
-                if(result[nextRow][nextCol] > arrivalTime) {
+                if(result[nextRow][nextCol] > arrivalTime){
                     result[nextRow][nextCol] = arrivalTime;
                     minHeap.push({arrivalTime, {nextRow, nextCol}});
                 }
---
+++ May-2025/weekly_contest_448/Maximum_product_of_two_digits.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Link: https://leetcode.com/problems/maximum-product-of-two-digits/
+
+int maxProduct(int n); // Leetcode-3536
+
+int main(){
+
+
+
+    return 0;
+}
+
+int maxProduct(int n){
+    int maxi = 0, smaxi = 0;
+
+    while(n != 0){  // Time complexity: O(digits)
+        int num = n%10;
+        if(maxi < num){
+            smaxi = maxi;
+            maxi = num;
+        }
+        else if(smaxi < num) smaxi = num;
+        n = n/10;
+    }
+    
+    return maxi*smaxi;
+}
