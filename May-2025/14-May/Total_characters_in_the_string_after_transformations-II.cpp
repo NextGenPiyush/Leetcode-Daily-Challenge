69bb4bd07afacc6bdb9ae37bfa0e873e2892f814
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-19 01:32:45 +0530

    Updated code

---
+++ May-2025/14-May/Total_characters_in_the_string_after_transformations-II.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthAfterTransformations(string s, int t, vector<int>& nums); // Leetcode-3337
+
+int main(){ 
+    
+    
+    
+    return 0;
+}
+
+int lengthAfterTransformations(string s, int t, vector<int>& nums){
+    
+}
