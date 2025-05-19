aca6b74321a296c33d38a7617fe1eeb0d08a09da
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-20 01:42:26 +0530

    Updated code

--- May-2025/18-May/Painting_a_grid_with_three_different_colors.cpp
+++ May-2025/18-May/Painting_a_grid_with_three_different_colors.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 
 // Problem Link: https://leetcode.com/problems/painting-a-grid-with-three-different-colors/?envType=daily-question&envId=2025-05-18
-int mod = 1e9 + 7;
-vector<vector<int>> dp;
 
+int mod = 1e9 + 7;
+
+vector<vector<int>> dp;  // Approach-1 
 int colorTheGrid(int m, int n); // Leetcode-1931
 void generateColumnState(string curr, char prevChar, int length, int rows, vector<string> &columnStates);
 int func(int remColumn, int prevIdx, vector<string> &columnStates);
 bool check(string prevState, string currState);
+
+
+vector<vector<int>> dp;  // Approach-2 Using bit-masking
+int colorTheGrid(int m, int n); // Leetcode-1931
+int solve(int &rows,int &cols,int currRow, int currCol ,int curr_state, int prev_state);
 
 int main(){
     
@@ -64,4 +70,48 @@ int func(int remColumn, int prevIdx, vector<string> &columnStates){
 bool check(string prevState, string currState){  // O(m) this check whether the current state can be present adjacent to the previous state of the column of the grid
     for(int i = 0; i < prevState.size(); i++) if(prevState[i] == currState[i]) return false;
     return true;
+}
+
+
+//******************************* Alternate approach using Bit-masking *******************************//
+
+int colorTheGrid(int m, int n){
+    dp.resize(1000+2, vector<int>(1023, -1)); // have taken maxspace which can be required in worst case
+    // dimension of memoization state = No of Columns * Color combinations per column So, dp = 1000 * 1023 size
+    
+    // Cell per column = No of rows = 5
+    // To represent 5 colors, each of 2 bits, total bits required = 10 bits
+    // Max value with 10 bits (all set-bits) = 1023
+
+    // 1023 --> 4^5 generalize as 4^m and 1000 generalize as n
+    // Time complexity: O(n * (4^m) * (2^m)) 
+    // n*(4^m) for total dp states computation and for each computation we have to traverse to all the rows of that column again and again to get the all possible ways
+    
+    return solve(m, n, 0, 0, 0, 0);
+}
+
+int countWays(int &rows,int &cols,int currRow, int currCol ,int curr_state, int prev_state){
+    if(currCol == cols) return 1;   // Valid Painting Combination
+
+    if(currRow == rows) return solve(rows, cols, 0, currCol + 1, 0, curr_state);  
+    //Goto next column --> currRow = 0, curr_state = 0, prev_state = curr_state
+
+    if(currRow == 0 && dp[currCol][prev_state] != -1) return dp[currCol][prev_state];
+    // if you are starting to make a new column then first check whether the ans is already computed for currCol and prev_state 
+
+    int up_color = 0;
+    if(currRow > 0) up_color = curr_state & 3;
+    int left_color = (prev_state >> ((rows - currRow - 1) * 2)) & 3;
+    // masking with 3 i.e 11 in binary, just gets the pair bits in which we are interested. This turns OFF more significant pair bits.
+
+    int ways = 0;
+    for(int color = 1; color <= 3; color++){  // Try all colors
+        if(color != up_color && color != left_color)
+            ways = (ways + solve(rows ,cols, currRow+1, currCol, (curr_state<<2) + color, prev_state))%mod;
+            // (curr_state<<2) + color --> this  is doing to add extra color 
+    }
+
+    if(currRow == 0) dp[currCol][prev_state] = ways;  // store only when currRow = 0
+
+    return ways;
 }
