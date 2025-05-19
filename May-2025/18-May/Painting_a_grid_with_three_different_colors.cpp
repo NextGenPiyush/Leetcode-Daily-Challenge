5427b68e587a79e83377a64bbb06179187283a47
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-05-19 22:37:38 +0530

    updated code

---
+++ May-2025/18-May/Painting_a_grid_with_three_different_colors.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mod = 1e9 + 7;
+vector<vector<int>> dp;
+unordered_map<string,string> t;
+
+int colorTheGrid(int m, int n); // Leetcode-1931
+void generateColumnState(string curr, char prevChar, int length, int rows, vector<string> &columnStates);
+int func(int remColumn, int prevIdx, vector<string> &columnStates);
+bool check(string prevState, string currState);
+
+int main(){
+    
+    
+    
+    return 0;
+}
+
+void generateColumnState(string curr, char prevChar, int length, int rows, vector<string> &columnStates){
+    if(length == rows){
+        columnStates.push_back(curr);  //  this function helps us to Generating all states of a column
+        return;
+    }
+
+    for(char ch : {'R', 'G', 'B'}){  // O(3)
+        if(ch == prevChar) continue;  // but this will run for 2 times only one iteration will be reduced due to this line so time complexity: O(2)
+        generateColumnState(curr+ch, ch, length+1, rows, columnStates);
+    }
+}
+
+int colorTheGrid(int m, int n){
+    vector<string> columnStates;  
+    generateColumnState("", '#', 0, m, columnStates);  // O(2^m) for generating all possible column states
+
+    dp.resize(n, vector<int>(columnStates.size(), -1));
+    // Total Time complexity: O(2^m + (n*m*t^2));
+    // space complexity: O(n*t) + O(t)
+
+    // let columnStates.size() == t 
+    int result = 0;
+    for(int i = 0; i < columnStates.size(); i++) result = (result%mod + func(n-1, i, columnStates)%mod)%mod;
+
+    return result;
+}
+
+int func(int remColumn, int prevIdx, vector<string> &columnStates){  
+    if(remColumn == 0) return 1;   
+    if(dp[remColumn][prevIdx] != -1) return dp[remColumn][prevIdx];
+     
+    int ways = 0;  // Time complexity: O((n*t) * (m*t)) an n*t total states of dp and m*t is the worst case for calling the bool check() function
+    string prevState = columnStates[prevIdx];
+
+    for(int i = 0; i < columnStates.size(); i++){  
+        if(i == prevIdx) continue; // as we can't take same state as previous
+        string currState = columnStates[i];
+        if(check(prevState, currState)) ways = (ways%mod + func(remColumn-1, i, columnStates)%mod)%mod;
+    }
+
+    return dp[remColumn][prevIdx] = ways;  
+}
+
+bool check(string prevState, string currState){  // O(m) this check whether the current state can be present adjacent to the previous state of the column of the grid
+    for(int i = 0; i < prevState.size(); i++) if(prevState[i] == currState[i]) return false;
+    return true;
+}
