9e0148bce4c424495c79f577ee3c110a5c351fa8
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-06-10 18:57:18 +0530

    Updated code

---
+++ June-2025/10-June/Max_diff_between_even_and_odd_frequency-I.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxDifference(string s); // Leetcode-3442
+
+int main(){
+    
+    
+    
+    return 0;
+}
+
+int maxDifference(string s){
+    vector<int> freq(26,0);  // space complexity: O(1)
+    for(auto ch : s) freq[ch-'a']++;  // time complexity: O(n)
+
+    int max_odd_freq = 0;
+    int min_even_freq = INT_MAX;
+
+    for(int i=0; i<26; i++){  // O(1)
+        if(freq[i] % 2 == 0 && freq[i] != 0) min_even_freq = min(min_even_freq, freq[i]);
+        else max_odd_freq = max(max_odd_freq, freq[i]);
+    }
+
+    if(min_even_freq == INT_MIN) return max_odd_freq;
+    return (max_odd_freq - min_even_freq);
+}
