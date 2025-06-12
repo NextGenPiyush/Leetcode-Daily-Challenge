37261e93cc749022b92043cf1fe1dfdde5d49e9a
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-06-12 22:17:03 +0530

    Updated code

---
+++ June-2025/12-June/Max_diff_btw_Adj_elements_in_a_circular_array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Link: https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/description/?envType=daily-question&envId=2025-06-12
+
+int maxAdjacentDistance(vector<int>& nums);  // Leetcode-3423
+
+int main(){
+
+
+
+    return 0;
+}
+
+int maxAdjacentDistance(vector<int>& nums){
+    int n = nums.size();
+    int max_abb_diff = INT_MIN;
+
+    for(int i=0; i<n-1; i++) max_abb_diff = max(max_abb_diff, abs(nums[i] - nums[i+1]));
+    max_abb_diff = max(max_abb_diff, abs(nums[0] - nums[n-1]));  // circular array 
+
+    return max_abb_diff;
+}
